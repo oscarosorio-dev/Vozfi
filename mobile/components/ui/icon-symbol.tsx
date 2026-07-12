ca26e93bee889494d8e6d267d353ba1a8dc8d005
Oscar Osorio <oscar.osorio1884@gmail.com>
2026-07-12 10:13:58 -0500

    fix: add missing icon mappings for mic, list, and chart symbols

--- mobile/components/ui/icon-symbol.tsx
+++ mobile/components/ui/icon-symbol.tsx
@@ -18,6 +18,9 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'mic.fill': 'mic',
+  'list.bullet': 'format-list-bulleted',
+  'chart.bar.fill': 'bar-chart',
 } as IconMapping;
 
 /**
